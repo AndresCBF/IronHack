3a7e5bcc4282096715afa71a1d53289f716150ca
AndresCBF <andrescamilob@gmail.com>
2022-02-05 15:07:10 -0300

    update

--- Projetos/ETL Tasty/Script Postgres/ScriptCreateTables.sql
+++ Projetos/ETL Tasty/Script Postgres/ScriptCreateTables.sql
@@ -1,19 +1,19 @@
-create database tasty;
+create database tastty;
 
-DROP TABLE IF EXISTS tasty.public.ingredients;
-CREATE TABLE tasty.public.ingredients (
+DROP TABLE IF EXISTS tastty.public.ingredients;
+CREATE TABLE tastty.public.ingredients (
 	Ingredient_id integer PRIMARY KEY,
 	Ingredient_name text
 	);
 
-DROP TABLE IF EXISTS tasty.public.measurements;
-CREATE TABLE tasty.public.measurements (
+DROP TABLE IF EXISTS tastty.public.measurements;
+CREATE TABLE tastty.public.measurements (
 	Unit_id integer PRIMARY KEY,
 	Unit_name text
 	);
 	
-DROP TABLE IF EXISTS tasty.public.nutrition;
-CREATE TABLE tasty.public.nutrition (
+DROP TABLE IF EXISTS tastty.public.nutrition;
+CREATE TABLE tastty.public.nutrition (
 	Nutrition_id integer PRIMARY KEY,
 	Recipe_calories integer,
 	Recipe_carbohydrates integer,
@@ -23,8 +23,8 @@ CREATE TABLE tasty.public.nutrition (
 	Recipe_sugar integer            
 	);
 	
-DROP TABLE IF EXISTS tasty.public.ratings;
-CREATE TABLE tasty.public.ratings (
+DROP TABLE IF EXISTS tastty.public.ratings;
+CREATE TABLE tastty.public.ratings (
 	Ratings_id integer PRIMARY KEY,
 	Recipe_score float,
 	Recipe_positivevotes integer,
@@ -32,14 +32,14 @@ CREATE TABLE tasty.public.ratings (
 	Recipe_totalvotes integer         
 	);
 	
-DROP TABLE IF EXISTS tasty.public.country;
-CREATE TABLE tasty.public.country (
+DROP TABLE IF EXISTS tastty.public.country;
+CREATE TABLE tastty.public.country (
 	Country_id integer PRIMARY KEY,
 	Country_tags text
 	);
 	
-DROP TABLE IF EXISTS tasty.public.recipes;
-CREATE TABLE Tasty.public.recipes (
+DROP TABLE IF EXISTS tastty.public.recipes;
+CREATE TABLE tastty.public.recipes (
 	Recipe_id integer PRIMARY KEY,
 	Recipe_name text,
 	Recipe_servings text,
@@ -48,13 +48,13 @@ CREATE TABLE Tasty.public.recipes (
 	Recipe_video text,
 	Recipe_tags text,
 	Country_id integer,
-	FOREIGN KEY (Country_id) REFERENCES tasty.public.country(Country_id),
-	FOREIGN KEY (Recipe_id) REFERENCES tasty.public.nutrition(Nutrition_id),
-	FOREIGN KEY (Recipe_id) REFERENCES tasty.public.ratings(Ratings_id)
+	FOREIGN KEY (Country_id) REFERENCES tastty.public.country(Country_id),
+	FOREIGN KEY (Recipe_id) REFERENCES tastty.public.nutrition(Nutrition_id),
+	FOREIGN KEY (Recipe_id) REFERENCES tastty.public.ratings(Ratings_id)
 	);
 	
-DROP TABLE IF EXISTS tasty.public.ingredientsquantitymeasurments;
-CREATE TABLE tasty.public.ingredientsquantitymeasurments (
+DROP TABLE IF EXISTS tastty.public.ingredientsquantitymeasurments;
+CREATE TABLE tastty.public.ingredientsquantitymeasurments (
 	Recipe_id integer,
 	Ingredient_id integer,
 	Unit_id integer,
@@ -62,7 +62,7 @@ CREATE TABLE tasty.public.ingredientsquantitymeasurments (
 	Ingredient_name text,
 	Unit_name text,
 	Ingredient_text text,
-	FOREIGN KEY (Recipe_id) REFERENCES tasty.public.recipes(Recipe_id), 
-	FOREIGN KEY (Ingredient_id) REFERENCES tasty.public.ingredients(Ingredient_id),
-	FOREIGN KEY (Unit_id) REFERENCES tasty.public.measurements(Unit_id)
+	FOREIGN KEY (Recipe_id) REFERENCES tastty.public.recipes(Recipe_id), 
+	FOREIGN KEY (Ingredient_id) REFERENCES tastty.public.ingredients(Ingredient_id),
+	FOREIGN KEY (Unit_id) REFERENCES tastty.public.measurements(Unit_id)
 	);
